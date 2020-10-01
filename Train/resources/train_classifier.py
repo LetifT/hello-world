a4b1cd0922a5a08656a34654c810e40d6d22daf6
jeroenR123 <jeroenraap@gmail.com>
2020-10-01 10:54:29 +0200

    Update: train_test_spit added and accuracy metric

--- Train/resources/train_classifier.py
+++ Train/resources/train_classifier.py
@@ -1,6 +1,8 @@
 
 # Train logistic regression model
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 from joblib import dump, load
 import os
 import pandas as pd
@@ -12,18 +14,20 @@ def train(clf):
     #data_repo = "hello-world\Data preprocessing\resources\data.json"
     df = pd.read_json(r"Data preprocessing\data.json", orient="split")
 
-    print(df)
     # Select features
     X = df.loc[:, df.columns != 'labels']
 
     # Select label
     y = df['labels']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, train_size= 0.9)
 
     #train model
-    clf = LogisticRegression().fit(X, y)
+    clf = LogisticRegression().fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
+    accuracy = accuracy_score(y_pred, y_test)
     dump(clf, 'clf.joblib')
-    return json.dumps({'message': 'The model was saved locally.'},
-                      sort_keys=False, indent=4), 200
+    return json.dumps({'message': 'The model was saved locally.',
+                       'accuracy test':  accuracy}, sort_keys=False, indent=4), 200
 
     """
     clf_api = os.environ['CLF_API']
